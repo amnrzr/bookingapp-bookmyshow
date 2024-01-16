b22f90c3ac01e3315ca7bc63ad70cd8074518990
amnrzr <vipajsn@outlook.com>
2024-01-16 15:06:48 +0530

    booking details

--- frontend/src/components/UI/LastBookingDetails.js
+++ frontend/src/components/UI/LastBookingDetails.js
@@ -4,46 +4,42 @@ import { useContext } from "react";
 import BsContext from "../../context/Context";
 import { seats } from "../../data";
 
-const LastBookingDetails = (props) => {
+const LastBookingDetails = () => {
   const context = useContext(BsContext);
-
   const { handleGetLastBooking, lastBookingDetails } = context;
 
   useEffect(() => {
-    // fetching the details of last booking
+    // Fetching the details of the last booking
     handleGetLastBooking();
-  });
+  }, []); // Ensure useEffect runs only once on component mount
 
   return (
     <div className="last_booking_details_container_main">
       <h2 className="last_booking_details_header">Last Booking Details:</h2>
       {lastBookingDetails ? (
-        // this will show the last booking details if avalible
+        // Show last booking details if available
         <>
           <div className="seats_container">
             <p className="seats_header">Seats:</p>
             <ul className="seats">
-              {seats.map((seat, index) => {
-                //this will map through seats arry and show it
-                return (
-                  <li className="seat_value" key={index}>
-                    {seat}: {Number(lastBookingDetails.seats[seat])}
-                  </li>
-                );
-              })}
+              {seats.map((seat, index) => (
+                <li className="seat_value" key={index}>
+                  {seat}: {lastBookingDetails.seats ? lastBookingDetails.seats[seat] || 0 : 0}
+                </li>
+              ))}
             </ul>
           </div>
           <p className="slot" style={{ textAlign: "left" }}>
-            {/* this will show the time slot of booking */}
+            {/* Show the time slot of the booking */}
             Slot: <span>{lastBookingDetails.slot}</span>
           </p>
           <p className="movie">
-            {/* show the movie name of last booked movie */}
+            {/* Show the movie name of the last booked movie */}
             Movie: <span>{lastBookingDetails.movie}</span>
           </p>
         </>
       ) : (
-        // display the message if the last boking details are not avaliable
+        // Display a message if the last booking details are not available
         <p className="no_previous_booking_msg">No Previous Booking Found!</p>
       )}
     </div>
